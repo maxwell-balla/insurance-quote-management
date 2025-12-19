8887d19a5a4dd32472fc9ad835d03cf42ab61e14
maxwell-balla <ballamaxwell7@gmail.com>
2025-12-19 16:38:20 +0400

    refactor: remove unused annotations and imports from StarterDevisApp and StarterPricingApp

--- pricing/starter-pricing/src/main/java/com/playground/starter_pricing/StarterPricingApp.java
+++ pricing/starter-pricing/src/main/java/com/playground/starter_pricing/StarterPricingApp.java
@@ -1,27 +1,12 @@
 package com.playground.starter_pricing;
 
-import com.playground.core_pricing.DomainPricingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = "com.playground.adapter_pricing.repository")
-@ComponentScan(
-        basePackages = {
-                "com.playground.starter_pricing",
-                "com.playground.adapter_pricing",
-                "com.playground.core_pricing"
-        },
-        includeFilters = @ComponentScan.Filter(
-                type = FilterType.ANNOTATION,
-                classes = DomainPricingService.class
-        )
-)
 public class StarterPricingApp {
-    public static void main( String[] args ) {
+
+    public static void main(String[] args) {
         SpringApplication.run(StarterPricingApp.class, args);
     }
 }
